87db246208d56da215798e00ed328936e10d0273
Carlos Munoz <carlosxmunoz@protonmail.com>
2020-08-02 19:22:04 -0500

    Updates

---
+++ precache-manifest.7d427c744b1de3d05d66874f957341cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a19bf7787d4193d1dcce409ff2cbc84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2410a6fb9b6676e32ff2",
+    "url": "/static/css/main.86bcdf05.chunk.css"
+  },
+  {
+    "revision": "8daf289850c1e1558b9c",
+    "url": "/static/js/2.1d1b8343.chunk.js"
+  },
+  {
+    "revision": "2410a6fb9b6676e32ff2",
+    "url": "/static/js/main.a225a88e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c721b23a8ba6d58b5835a9efa136d30c",
+    "url": "/static/media/jellyfish.c721b23a.svg"
+  }
+]);
